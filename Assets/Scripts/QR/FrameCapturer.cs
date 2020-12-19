f83222589a0747186cbfadc617f8525c70b26295
Grzegorz Otworowski <grzegorz.otw@gmail.com>
2020-12-19 17:04:59 +0100

    working qr code scanner

---
+++ Assets/Scripts/QR/FrameCapturer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameCapturer : MonoBehaviour
+{
+
+    // Script Inputs
+    public bool m_shouldCaptureOnNextFrame = false;
+    public Color32[] m_lastCapturedColors;
+    // Privates
+    Texture2D m_centerPixTex;
+
+    void Start()
+    {
+        Resolution currentResolution = Screen.currentResolution;
+        m_centerPixTex = new Texture2D(currentResolution.width, currentResolution.height, TextureFormat.RGBA32, false);
+    }
+
+    void OnPostRender()
+    {
+
+        if (m_shouldCaptureOnNextFrame)
+        {
+            Resolution res = Screen.currentResolution;
+            m_lastCapturedColors = GetRenderedColors();
+            m_shouldCaptureOnNextFrame = false;
+        }
+    }
+
+    // Helpers
+    Color32[] GetRenderedColors()
+    {
+        Resolution currentResolution = Screen.currentResolution;
+        m_centerPixTex.ReadPixels(new Rect(0, 0, currentResolution.width, currentResolution.height), 0, 0);
+        m_centerPixTex.Apply();
+
+        return m_centerPixTex.GetPixels32();
+    }
+    
+}
---
+++ Assets/Scripts/QR/ScannerQR.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+using ZXing;
+using ZXing.QrCode;
+using UnityEngine.UI;
+
+public class ScannerQR : MonoBehaviour
+{
+    public Camera cam;
+    public Text debugText;
+    private BarcodeReader barCodeReader;
+    
+
+    FrameCapturer m_pixelCapturer;
+
+    // Use this for initialization
+    void Start()
+    {
+        barCodeReader = new BarcodeReader();
+        Resolution currentResolution = Screen.currentResolution;
+        m_pixelCapturer = cam.GetComponent<FrameCapturer>();
+    }
+
+    void Update()
+    {
+
+        Resolution currentResolution = Screen.currentResolution;
+
+        try
+        {
+            Color32[] framebuffer = m_pixelCapturer.m_lastCapturedColors;
+            if (framebuffer.Length == 0)
+            {
+                //debugText.text = "0";
+                return;
+            }
+
+            var data = barCodeReader.Decode(framebuffer, currentResolution.width, currentResolution.height);
+            //debugText.text = "data";
+            if (data != null)
+            {
+                // QRCode detected.
+                debugText.text = "1";
+                Debug.Log(data);
+                string qr = "QR " + data.Text;
+                Debug.Log(qr);
+                debugText.text = qr;
+            }
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error reading QR");
+            Debug.LogError(e.Message);
+        }
+
+        // skip 1 frame each time 
+        // solves GetPixels() blocks for ReadPixels() to complete
+        // https://medium.com/google-developers/real-time-image-capture-in-unity-458de1364a4c
+        m_pixelCapturer.m_shouldCaptureOnNextFrame = true;
+    }
+}
