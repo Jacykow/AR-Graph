827162458e229753519327571307990d812c78ee
Jacykow <jacgulij@gmail.com>
2020-12-19 10:39:38 +0100

    wip

---
+++ Assets/Scripts/Presentation/Controllers/AR/ArAnchorController.cs
@@ -0,0 +1,50 @@
+﻿using GoogleARCore;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArAnchorController : MonoBehaviour
+{
+    [SerializeField]
+    private ArGraphVisualizer augmentedImageVisualizerPrefab;
+
+    private List<AugmentedImage> tempAugmentedImages = new List<AugmentedImage>();
+    private ArGraphVisualizer visualizer;
+
+    public void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            Screen.sleepTimeout = SleepTimeout.SystemSetting;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        Session.GetTrackables(tempAugmentedImages, TrackableQueryFilter.Updated);
+
+        foreach (var image in tempAugmentedImages)
+        {
+            if (image.TrackingState == TrackingState.Tracking && visualizer == null)
+            {
+                Anchor anchor = image.CreateAnchor(image.CenterPose);
+                visualizer = Instantiate(augmentedImageVisualizerPrefab, anchor.transform);
+            }
+            else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
+            {
+                Destroy(visualizer.gameObject);
+                visualizer = null;
+            }
+        }
+    }
+}
