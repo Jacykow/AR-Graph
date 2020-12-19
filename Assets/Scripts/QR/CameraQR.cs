9b5bceaf139be4c5d863312bed3d12009080e732
Grzegorz Otworowski <grzegorz.otw@gmail.com>
2020-12-19 10:43:34 +0100

    BasicQR

--- Assets/Scripts/QR/CameraQR.cs
+++ Assets/Scripts/QR/CameraQR.cs
@@ -1,20 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System;
 using ZXing;
 using ZXing.QrCode;
 
-private WebCamTexture camTexture;
-private Rect screenRect;
 
-public class Scan : MonoBehaviour
+public class CameraQR : MonoBehaviour
 {
+    private WebCamTexture camTexture;
+    private Rect screenRect;
+    public Quaternion baseRotation;
+
     void Start()
     {
         screenRect = new Rect(0, 0, Screen.width, Screen.height);
         camTexture = new WebCamTexture();
         camTexture.requestedHeight = Screen.height;
         camTexture.requestedWidth = Screen.width;
+        //baseRotation = transform.rotation;
         if (camTexture != null)
         {
             camTexture.Play();
@@ -25,6 +27,7 @@ public class Scan : MonoBehaviour
     {
         // drawing the camera on screen
         GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
+        // transform.rotation = baseRotation * Quaternion.AngleAxis(camTexture.videoRotationAngle, Vector3.back);
         // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
         try
         {
@@ -34,7 +37,7 @@ public class Scan : MonoBehaviour
               camTexture.width, camTexture.height);
             if (result != null)
             {
-                Debug.Log(“DECODED TEXT FROM QR: “ +result.Text);
+                Debug.Log("DECODED TEXT FROM QR: "+result.Text);
             }
         }
         catch (Exception ex) { Debug.LogWarning(ex.Message); }
