da91edc996664b4374a515850d92caef9acdb62c
Jacykow <jacgulij@gmail.com>
2020-12-19 12:48:24 +0100

    removed settings from controller

--- Assets/Scripts/Presentation/Controllers/AR/ArAnchorController.cs
+++ Assets/Scripts/Presentation/Controllers/AR/ArAnchorController.cs
@@ -1,39 +1,17 @@
 ﻿using GoogleARCore;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class ArAnchorController : MonoBehaviour
 {
     [SerializeField]
     private GameObject graphPrefab;
-    [SerializeField]
-    private TextMeshProUGUI debugText;
 
     private List<AugmentedImage> augmentedImages = new List<AugmentedImage>();
     private ArGraphVisualizer graphVisualizer;
 
-    public void Awake()
-    {
-        Application.targetFrameRate = 60;
-    }
-
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-
-        if (Session.Status != SessionStatus.Tracking)
-        {
-            Screen.sleepTimeout = SleepTimeout.SystemSetting;
-        }
-        else
-        {
-            Screen.sleepTimeout = SleepTimeout.NeverSleep;
-        }
-
         Session.GetTrackables(augmentedImages, TrackableQueryFilter.Updated);
 
         foreach (var augmentedImage in augmentedImages)
@@ -49,7 +27,5 @@ public class ArAnchorController : MonoBehaviour
                 Destroy(graphVisualizer.gameObject);
             }
         }
-
-        debugText.text = $"{augmentedImages.Count}\n{graphVisualizer}";
     }
 }
