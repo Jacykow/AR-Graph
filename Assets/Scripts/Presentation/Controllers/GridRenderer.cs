f57a91ffe103d52acbe002d16e1f0244c7cfc362
Agere98 <44338036+Agere98@users.noreply.github.com>
2020-12-28 20:36:58 +0100

    Fix UVs in grid renderer

--- Assets/Scripts/Presentation/Controllers/GridRenderer.cs
+++ Assets/Scripts/Presentation/Controllers/GridRenderer.cs
@@ -10,6 +10,19 @@ public class GridRenderer : MonoBehaviour
         meshRenderer = GetComponent<MeshRenderer>();
     }
 
+    //Flips the UV on the backside of the cube so it matches the front
+    private void OnValidate()
+    {
+        var uvs = GetComponent<MeshFilter>().mesh.uv;
+
+        uvs[6] = new Vector2(0, 0);
+        uvs[7] = new Vector2(1, 0);
+        uvs[10] = new Vector2(0, 1);
+        uvs[11] = new Vector2(1, 1);
+
+        GetComponent<MeshFilter>().sharedMesh.uv = uvs;
+    }
+
     public void Redraw(IAxisProperties primaryAxis, IAxisProperties secondaryAxis)
     {
         var height = primaryAxis.Length;
