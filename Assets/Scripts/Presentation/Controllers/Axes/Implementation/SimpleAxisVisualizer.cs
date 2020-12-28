d1e94340512c031d266f6c1dc2050e0e3142cfd8
Agere98 <44338036+Agere98@users.noreply.github.com>
2020-12-28 18:19:18 +0100

    Refactor axis rendering

--- Assets/Scripts/Presentation/Controllers/Axes/Implementation/SimpleAxisVisualizer.cs
+++ Assets/Scripts/Presentation/Controllers/Axes/Implementation/SimpleAxisVisualizer.cs
@@ -6,8 +6,9 @@ public class SimpleAxisVisualizer : MonoBehaviour, IShowable
     [Serializable]
     private class Axis : IAxisProperties
     {
-        [SerializeField] private AxisRenderer renderer;
         [SerializeField] private float length = 1f;
+
+        public AxisRenderer Renderer { get; set; }
 
         public Vector3 Direction { get; }
         public float Length => length;
@@ -19,17 +20,33 @@ public class SimpleAxisVisualizer : MonoBehaviour, IShowable
 
         public void Show()
         {
-            renderer.Redraw(this);
+            Renderer?.Redraw(this);
         }
     }
 
     [Header("Axes")]
+
+    [SerializeField] private GameObject prefab;
 
     [SerializeField] private Axis xAxis = new Axis(Vector3.right);
 
     [SerializeField] private Axis yAxis = new Axis(Vector3.up);
 
     [SerializeField] private Axis zAxis = new Axis(Vector3.forward);
+
+    private void Awake()
+    {
+        if (prefab != null && prefab.GetComponent<AxisRenderer>() != null)
+        {
+            AxisRenderer AxisRenderer() =>
+                Instantiate(prefab, transform.position, Quaternion.identity, transform)
+                    .GetComponent<AxisRenderer>();
+
+            xAxis.Renderer = AxisRenderer();
+            yAxis.Renderer = AxisRenderer();
+            zAxis.Renderer = AxisRenderer();
+        }
+    }
 
     public void Hide()
     {
