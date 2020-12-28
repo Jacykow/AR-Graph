e33ada35b64f51a4b49d7a04bf8b980f338d2ba5
Jacykow <jacgulij@gmail.com>
2020-12-28 14:38:27 +0100

    wip

---
+++ Assets/Scripts/Presentation/Controllers/AR/ArSurfaceController.cs
@@ -0,0 +1,37 @@
+﻿using GoogleARCore;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ArSurfaceController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject graphPrefab;
+
+    private void Update()
+    {
+        TryPlacePrefab();
+    }
+
+    private void TryPlacePrefab()
+    {
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+        {
+            return;
+        }
+
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+            TrackableHitFlags.FeaturePointWithSurfaceNormal;
+        if (!Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out TrackableHit hit))
+        {
+            return;
+        }
+
+        var gameObject = Instantiate(graphPrefab, hit.Pose.position, hit.Pose.rotation);
+    }
+}
