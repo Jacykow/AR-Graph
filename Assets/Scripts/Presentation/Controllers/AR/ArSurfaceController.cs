58f42ca18dd2307bdf4d0dcc20812410f98f5aaf
Jacykow <jacgulij@gmail.com>
2021-01-02 15:20:42 +0100

    added SurfaceScene

--- Assets/Scripts/Presentation/Controllers/AR/ArSurfaceController.cs
+++ Assets/Scripts/Presentation/Controllers/AR/ArSurfaceController.cs
@@ -7,10 +7,14 @@ public class ArSurfaceController : MonoBehaviour
     [SerializeField]
     private GameObject graphPrefab;
 
+    private ArGraphVisualizer graph;
+
     private void Update()
     {
-        TryPlacePrefab();
-        LostTracking();
+        if (graph == null)
+        {
+            TryPlacePrefab();
+        }
     }
 
     private void TryPlacePrefab()
@@ -20,14 +24,12 @@ public class ArSurfaceController : MonoBehaviour
             return;
         }
 
-        Debug.Log("asdf 1");
         var touch = Input.GetTouch(0);
 
         if (touch.phase != TouchPhase.Began || EventSystem.current.IsPointerOverGameObject(touch.fingerId))
         {
             return;
         }
-        Debug.Log("asdf 2");
 
         var raycastHitFlags = TrackableHitFlags.PlaneWithinPolygon |
             TrackableHitFlags.FeaturePointWithSurfaceNormal;
@@ -35,18 +37,7 @@ public class ArSurfaceController : MonoBehaviour
         {
             return;
         }
-        Debug.Log("asdf 3");
 
-        var gameObject = Instantiate(graphPrefab, hit.Pose.position, hit.Pose.rotation);
-        Debug.Log("asdf 4");
-    }
-
-    private void LostTracking()
-    {
-        if (Session.Status == SessionStatus.LostTracking && Session.LostTrackingReason != LostTrackingReason.None)
-        {
-            Debug.Log($"asdf tracking: {Session.LostTrackingReason}");
-        }
-        return;
+        graph = Instantiate(graphPrefab, hit.Pose.position, hit.Pose.rotation).GetComponent<ArGraphVisualizer>();
     }
 }
