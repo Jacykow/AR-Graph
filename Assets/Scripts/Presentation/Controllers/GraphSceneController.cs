9ad44b5e5e489132cf16e58648ba2b286f0633b6
ewelina <ewelinaba98@wp.pl>
2021-01-09 16:42:04 +0100

    add random chart button to UI

--- Assets/Scripts/Presentation/Controllers/GraphSceneController.cs
+++ Assets/Scripts/Presentation/Controllers/GraphSceneController.cs
@@ -1,13 +1,16 @@
-﻿using UniRx;
+﻿using System;
+using UniRx;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Random = System.Random;
 
 public class GraphSceneController : MonoBehaviour
 {
     public Button arOnPaperCard;
     public Button arInSpace;
     public Button space3D;
+    public Button randomChart;
     public Button scannerButton;
     public Image qrScanner;
 
@@ -16,6 +19,7 @@ public class GraphSceneController : MonoBehaviour
     private void Start()
     {
         _lastSelected = EventSystem.current.firstSelectedGameObject;
+        Random random = new Random();
 
         arOnPaperCard.OnClickAsObservable().Subscribe(_ =>
        {
@@ -33,6 +37,26 @@ public class GraphSceneController : MonoBehaviour
         {
             _lastSelected = space3D.gameObject;
             SetVisualisationType(VisualisationType.Space3D);
+        }).AddTo(this);
+
+        randomChart.OnClickAsObservable().Subscribe(_ =>
+        {
+            var types = Enum.GetValues(typeof(VisualisationType));
+            var randomType = (VisualisationType)types.GetValue(random.Next(types.Length));
+
+            SetVisualisationType(randomType);
+            if (randomType.Equals(VisualisationType.ArInSpace))
+            {
+                _lastSelected = arInSpace.gameObject;
+            }
+            else if (randomType.Equals(VisualisationType.ArOnPaperCard))
+            {
+                _lastSelected = arOnPaperCard.gameObject;
+            }
+            else
+            {
+                _lastSelected = space3D.gameObject;
+            }
         }).AddTo(this);
 
         scannerButton.gameObject.SetActive(false);
