b395af75b7e4e7ce475cb426567956d3910a55b0
Agere98 <44338036+Agere98@users.noreply.github.com>
2021-01-09 15:50:31 +0100

    Add title and axis labels

--- Assets/Scripts/Presentation/Controllers/Axes/Implementation/SimpleAxisRenderer.cs
+++ Assets/Scripts/Presentation/Controllers/Axes/Implementation/SimpleAxisRenderer.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
 public class SimpleAxisRenderer : AxisRenderer
 {
     [SerializeField] private float coneOffset = 0.1f;
+    [SerializeField] private GameObject labelPrefab;
+    [SerializeField] private float labelOffset = 0.2f;
 
     private LineRenderer lineRenderer;
     private Transform cone;
+    private TMP_Text label;
 
     private void Awake()
     {
@@ -18,8 +22,22 @@ public class SimpleAxisRenderer : AxisRenderer
     {
         var end = properties.Direction * (properties.Length + coneOffset);
         lineRenderer.positionCount = 2;
-        lineRenderer.SetPositions(new[] { transform.position, transform.position + transform.rotation * Vector3.Scale(end, transform.lossyScale) });
+        lineRenderer.SetPositions(new[] { transform.position, transform.TransformPoint(end) });
         cone.localPosition = end;
         cone.localRotation = Quaternion.LookRotation(properties.Direction);
+        DrawLabel(properties);
+    }
+
+    private void DrawLabel(IAxisProperties properties)
+    {
+        if (label == null)
+        {
+            var parent = GameObject.FindWithTag("Graph Labels");
+            if (labelPrefab == null || parent == null) return;
+            label = Instantiate(labelPrefab, transform.position, transform.rotation, parent.transform).GetComponent<TMP_Text>();
+        }
+        var labelPosition = properties.Direction * (properties.Length + labelOffset);
+        label.transform.position = transform.TransformPoint(labelPosition);
+        label.text = properties.Name;
     }
 }
