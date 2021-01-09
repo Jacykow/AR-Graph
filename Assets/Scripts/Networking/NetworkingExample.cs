239d1b8010f16c956f727596c01f2f075b8749a3
Grzegorz Otworowski <grzegorz.otw@gmail.com>
2021-01-09 18:50:46 +0100

    JsonBackendProcessing

--- Assets/Scripts/Networking/NetworkingExample.cs
+++ Assets/Scripts/Networking/NetworkingExample.cs
@@ -1,4 +1,5 @@
-﻿using UniRx;
+﻿using Newtonsoft.Json;
+using UniRx;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -6,8 +7,9 @@ public class NetworkingExample : MonoBehaviour
 {
     private void Start()
     {
+        ExamplePOST();
+        //ExampleGET();
     }
-
     private void ExampleGET()
     {
         new UnityWebRequest
@@ -17,13 +19,28 @@ public class NetworkingExample : MonoBehaviour
         }.ObserveRequestResult().Subscribe(text =>
         {
             Debug.Log(text);
+            var deserialized = JsonConvert.DeserializeObject<BackendData>(text);
+            var desContainer = JsonConvert.DeserializeObject<GraphDataContainer>(deserialized.data);
+            Debug.Log(desContainer);
         }).AddTo(this);
     }
 
     private void ExamplePOST()
     {
+        var randomdata = TestGraphVisualizationData.Pie2D;
+        var container = new GraphDataContainer
+        {
+            visualizationData = randomdata
+        };
+        var jsonString = JsonConvert.SerializeObject(container);
+
+        var graph = new BackendData(1, jsonString);
+        var secondJsonString = JsonConvert.SerializeObject(graph);
+
+        Debug.Log(secondJsonString);
+
         DataManager.Main
-            .SendGraph("{\"id\": 1, \"data\": \"przykladowe dane do grafu\"}")
+            .SendGraph(secondJsonString)
             .Subscribe(text =>
         {
             Debug.Log(text);
