79e22a881a240a30e3411ea3447802993d451239
Jacykow <jacgulij@gmail.com>
2021-01-20 19:49:17 +0100

    added column 2d colors

--- Assets/Scripts/Presentation/Controllers/Graphs/Implementation/ColumnGraph2DVisualizer.cs
+++ Assets/Scripts/Presentation/Controllers/Graphs/Implementation/ColumnGraph2DVisualizer.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Scripts.BLL.Models.GraphData.Implementation;
+using System.Linq;
 using UnityEngine;
 
 public class ColumnGraph2DVisualizer : BaseGraphVisualizer<ColumnGraph2DData>
@@ -8,6 +9,9 @@ public class ColumnGraph2DVisualizer : BaseGraphVisualizer<ColumnGraph2DData>
     protected override void Redraw(ColumnGraph2DData graphData)
     {
         graphRenderer.material.SetFloatArray("_Columns", graphData.Values);
+        // todo mati
+        var colors = graphData.Values.Select(value => Color.HSVToRGB(value, 1, 1)).ToArray();
+        graphRenderer.material.SetColorArray("_Colors", colors);
         graphRenderer.material.SetFloat("_ColumnAmount", graphData.Values.Length);
     }
 }
